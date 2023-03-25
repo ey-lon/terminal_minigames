73956edec42d142ec809efdc533766f63f5edbd8
Eylon_home <abettini@DESKTOP-10N313C>
2023-03-25 16:34:30 +0100

    Update guess_n

--- src/game.c
+++ src/game.c
@@ -20,11 +20,11 @@ void	ft_game(t_opt opt)
 	int			n;
 	int			att;
 
-	ft_printf("GAME ON!\n");
 	str = NULL;
 	att = 0;
 	srand(time(0));
 	n = rand() % (opt.max + 1 - opt.min) + opt.min;
+	ft_printf("GAME ON!\n");
 	while (1)
 	{
 		att++;
