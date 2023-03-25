5b89b32752087c0071817ee05ebac617a9ea24ff
Eylon_home <abettini@DESKTOP-10N313C>
2023-03-25 18:29:49 +0100

    Update guess_n

--- src/guess_n.h
+++ src/guess_n.h
@@ -28,7 +28,7 @@ void	ft_rules(t_opt opt);
 // ---------------------------------------
 void	ft_options(t_opt *opt);
 void	ft_set_range(int *max, int *min);
-void	ft_set_att_limit(int *n);
+void	ft_set_attempts(int *n);
 // ---------------------------------------
 int		ft_err_check(char *str);
 // ---------------------------------------
